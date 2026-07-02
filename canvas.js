0fda6fe98b46c7124b69b783d4a9fa5638ab4d68
FelipeMonteiroDeAssis <laipemorteiro@gmail.com>
2026-07-02 17:23:46 -0300

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -2,8 +2,8 @@ const eCanvas = document.querySelector("canvas");
 const eCtx = eCanvas.getContext("2d");
 
 function setup(){
-    eCanvas.width = eCtx.width = screen.width;
-    eCanvas.height = eCtx.height = screen.height;
+    eCtx.width = screen.width;
+    eCtx.height = screen.height;
 }
 setup();
 
